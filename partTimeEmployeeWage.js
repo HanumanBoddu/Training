b9b9ea3f6b46d5ce5898b963d1e8fc4633678433
Hanuman Boddu <Hanuman.Boddu@godigit.com>
2024-07-23 14:45:56 +0530

    Add partTimeEmployeeWage.js with generateAttendance function

---
+++ partTimeEmployeeWage.js
@@ -0,0 +1,8 @@
+function partTimeWagecalculation(generatedAttendance) {
+    var totalHours = 4;
+    var wage = totalHours * 20;
+    console.log(`Total Wage: ${wage}`);
+    return wage;
+}
+
+partTimeWagecalculation();
