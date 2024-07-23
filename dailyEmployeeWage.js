2e7241faa94d405fc2e0c533a82c4dc6394fd4d9
Hanuman Boddu <Hanuman.Boddu@godigit.com>
2024-07-23 14:31:10 +0530

    Add dailyEmployeeWage.js with generateAttendance function

---
+++ dailyEmployeeWage.js
@@ -0,0 +1,9 @@
+function dailyWageCalculation(){
+    var workingHours=8;
+    var wagePerHour=20;
+    var dailyWage=workingHours*wagePerHour;
+    console.log("Daily Employee Wage:"+dailyWage);
+    return dailyWage; 
+}
+
+dailyWageCalculation(); // calling the function from dailyEmployeeWage.js
